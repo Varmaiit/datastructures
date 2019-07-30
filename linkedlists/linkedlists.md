abdb104c1a0bb3b23a5cf5f6f04d53d05a9d4f4f
Chaitanya Varma <varma.iit@gmail.com>
2019-07-30 21:28:35 +0530

    implementation of reversing of linked lists and finding nth from end

--- linkedlists/linkedlists.md
+++ linkedlists/linkedlists.md
@@ -5,13 +5,25 @@ Reference: Data-Structures-and-Algorithms-Made-Easy-Data-Structures-and-Algorith
 Linked lists (ADT)
 ------------------
 
-# Main operations
+### Main operations
  - Insert, Delete
 
-# Auxiliary operations
+### Auxiliary operations
  - Delete List, Count, Find nth node from the end of the list
 
 
 Complexity
 -----------
 Refer to page 78: Data-Structures-and-Algorithms-Made-Easy-Data-Structures-and-Algorithmic-Puzzles
+
+
+Doubly linked list
+------------------
+
+### Advantages
+- It has the address of both previous node and next node, So unlike linked list we need not have a pointer to the previous node in order to delete it
+
+
+### Disadvantages
+ - Each node requires an extra pointer, requiring more space
+ - insertion or deletion of a node takes little longer, because of more pointer operations.
--- linkedlists/singlelinkedlist.py
+++ linkedlists/singlelinkedlist.py
@@ -76,16 +76,17 @@ class LinkedList(object):
         temp.next_node =None
         previous_node.next_node =next_node
 
-
-
-
-        
-
-
-        
-
+    def reverse_list(self):
+        prev_node = None
+        cur_node = self.head
+        while(cur_node is not None):
+            next_node = cur_node.next_node
+            cur_node.next_node = prev_node
+            prev_node = cur_node
+            cur_node = next_node
+        self.head = prev_node
     
-if __name__=='__main__': 
+def deleting_the_node_at_nth_position():
     "Code execution......"
     llist = LinkedList()
     llist.insert(3)
@@ -99,6 +100,35 @@ if __name__=='__main__':
     print("printing the new linked list")
     llist.traverse()
 
+
+
+
+        
+
+def deleting_the_node_at_nth_position_from_the_end(position):
+    """ step1 : traverse one pointer to the nth positon
+        step2 : now start the other pointer from 1st position 
+        step3 : now start traversing the both the pointers along with a counter till the 1st pointer reaches last node.
+        step4 : now having the position for nth node from the end, now one can delete that """
+
+
+
+if __name__=='__main__': 
+    "Code execution......"
+    llist = LinkedList()
+    llist.insert(3)
+    llist.insert(4)
+    llist.insert(9)
+    llist.insert(34)
+    print("printing the linked list")
+    llist.traverse()
+    print("deleting the node at position 2")
+    llist.delete(2)
+    print("printing the new linked list")
+    llist.traverse()
+    print("printing the new reverse linked list")
+    llist.reverse_list()
+    llist.traverse()
 
 
 
